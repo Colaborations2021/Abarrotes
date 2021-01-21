518444dceee3793ea818eeaa1c75d8cc79476e18
Victor Lascares <vmlascares@gmail.com>
2021-01-20 23:09:45 -0600

    Agregando controladores de Frames Administrador, Venta, Principal, Trabajador y LogIn

--- src/Controlador/controladorFrmAdministrador.java
+++ src/Controlador/controladorFrmAdministrador.java
@@ -1,18 +1,14 @@
-package Controlador;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Controlador;
 
 /**
  *
- * @author Axel
+ * @author lasca
  */
-public class Principal {
-    public static void main(String[] args) {
-        
-        
-    }
+public class controladorFrmAdministrador {
+    
 }
---
+++ src/Controlador/controladorPrincipal.java
@@ -0,0 +1,8 @@
+package Controlador;
+
+public class controladorPrincipal {
+    public static void main(String[] args) {
+        
+        
+    }
+}
